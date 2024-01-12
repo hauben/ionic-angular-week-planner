5842a71dfc45298259d834d71661bdaadd5b20d8
Frank Haubenschild <frank@haubenschild.de>
2024-01-12 18:18:39 +0100

    App Icons

--- android/app/src/main/java/de/weekplanner/MainActivity.java
+++ android/app/src/main/java/de/weekplanner/MainActivity.java
@@ -1,4 +1,4 @@
-package io.ionic.starter;
+package de.weekplanner;
 
 import com.getcapacitor.BridgeActivity;
 
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'IonicWeekPlanner',
+  appId: 'de.weekplanner',
+  appName: 'Week Planner',
   webDir: 'www',
   server: {
     androidScheme: 'https'
